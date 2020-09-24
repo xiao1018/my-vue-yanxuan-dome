9420743993d5e7b32d3d1c9feb0864b9f79f06bb
xiao1018 <xiao1018@126.com>
2020-09-25 00:59:52 +0800

    修改了store

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import homeStore from './modules/home'
-import cartStore from './modules/cart'
-import categoryStore from './modules/category'
-import topicStore from './modules/topic'
-import mineStore from './modules/mine'
+import home from './modules/home'
+import cart from './modules/cart'
+import category from './modules/category'
+import topic from './modules/topic'
+import mine from './modules/mine'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    homeStore,
-    cartStore,
-    categoryStore,
-    topicStore,
-    mineStore
+    home,
+    cart,
+    category,
+    topic,
+    mine
   }
 })
